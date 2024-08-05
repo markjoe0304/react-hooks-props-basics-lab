026f7b0f44b6ef653f6c570df8d3dfc318f75288
markjoe0304 <markjoegicheru@gmail.com>
2024-08-05 10:09:17 +0300

    modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Links from "./Links";
+import Links from "./links.js";
 
 function About({bio, github, linkedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && <p>{props.bio}</p>}
+      {bio && bio.trim() !== "" && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={github} linkedin={linkedin}/>
     </div>
